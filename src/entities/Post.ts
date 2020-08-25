6715c0a1e85bbeffdbb4aa6d55846a49d7d09414
szy0syz <szy0syz@gmail.com>
2020-08-25 23:06:01 +0800

    Update MikroORM TypeGraphQL Crud

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,23 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { ObjectType, Field, Int } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
-  @Property({ type: 'text' })
-  title!: string;
-
-  @Property({ type: 'date' })
+  @Field(() => String)
+  @Property({ type: "date" })
   createdAt = new Date();
 
-  @Property({ type: 'date', onUpdate: () => new Date() })
+  @Field(() => String)
+  @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
+
+  // @Field()  如果这里不注入 Field 则 GraphQL 拿不到该字段
+  @Field()
+  @Property({ type: "text" })
+  title!: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
-import { HelloResolver } from './resolvers/hello';
+import "reflect-metadata";
+import { PostResolver } from './resolvers/post';
 import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
 import microConfig from './mikro-orm.config';
-import epxress from 'express';
+import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-  await orm.getMigrator().up();
+  // await orm.getMigrator().up();
 
-  const app = epxress();
+  const app = express();
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
   apolloServer.applyMiddleware({ app });
 
-  app.listen(4000, () =>
-    console.log('server started on http://localhost:4000')
-  );
-
-  // const post = orm.em.create(Post, { title: 'my first post' });
-  // await orm.em.persistAndFlush(post);
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  app.listen(4000, () => {
+    console.log('server started on http://localhost:4000');
+  });
 };
 
 main().catch((err) => {
